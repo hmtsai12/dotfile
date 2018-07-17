ddd66fb105f523112509a3fe68551f0e3e91021e
hmtasi12 <hmtsai12@yahoo.com.tw>
2018-07-18 07:51:40 +0800

    add expect folder function

--- gettrack.sh
+++ gettrack.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 ctags -R
-find -depth -type d | grep -v 'svn' > test.vim
+find -depth -type d | egrep -v '\./.git|\./SDK' > test.vim
 sed 's/^\.\//set path+=/' test.vim > workspace.vim
 rm test.vim
-find -type f | egrep '\.[ch]$|\.cc$|\.cpp$' > cscope.files
+find -type f | egrep '\.[ch]$|\.cc$|\.cpp$|\.mk$' > cscope.files.out
+egrep -v '\./.git|\./SDK' cscope.files.out > cscope.files
 cscope -bq
-
+rm cscope.files.out
